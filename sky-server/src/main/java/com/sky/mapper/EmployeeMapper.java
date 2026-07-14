a9c9e8e551a52f259dd54d8d19977768f6b40cb7
sky-take-out <ch849267941@qq.com>
2026-07-14 20:35:33 +0800

    苍穹外卖源代码+添加员工+员工分页查询

---
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -0,0 +1,23 @@
+package com.sky.mapper;
+
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
+import com.sky.entity.Employee;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface EmployeeMapper {
+
+    /**
+     * 根据用户名查询员工
+     * @param username
+     * @return
+     */
+    @Select("select * from employee where username = #{username}")
+    Employee getByUsername(String username);
+
+    void insert(Employee employee);
+
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+}
